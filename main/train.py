976a5277e65fee2e5376edb743b3d34f3763dfea
109598089 <t109598089@ntut.org.tw>
2021-12-14 16:36:51 +0800

    change training epochs

--- main/train.py
+++ main/train.py
@@ -63,7 +63,7 @@ def run_train(tableNumber):
     )
     #########
 
-    for epoch in range(config.EPOCHS):
+    for epoch in range(10):
         outputs, targets, loss = engine.train_fn(encoder_data_loader, decoder_data_loader, model, optimizer, device, scheduler)
         print(f'Epoch:{epoch+1}, Loss:{loss:.4f}')
 
